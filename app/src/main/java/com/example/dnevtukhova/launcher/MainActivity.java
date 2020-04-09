ffa2eb373ab67b11d36120bd77d3c236b301cf28
Максим <max1171@yandex.ru>
2020-04-09 23:42:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dnevtukhova/launcher/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dnevtukhova.launcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
